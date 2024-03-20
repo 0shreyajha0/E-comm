754dea3e419fc6da027a2bd2ec9ce227a362c0b3
0shreyajha0 <0shreyajha0@gmail.com>
2024-03-20 12:20:05 +0530

    updated Navbar

--- src/components/DynamicNavbar.js
+++ src/components/DynamicNavbar.js
@@ -1,15 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import '../styles/dyanimicmenu.css'
-import a from '../assests/Logo.png'
+import React, { useState, useEffect } from "react";
+import "../styles/dyanimicmenu.css";
+import a from "../assests/Logo.png";
 
 const DynamicNavigationMenu = ({ menuItems }) => {
   const [visibleItems, setVisibleItems] = useState([]);
   const [hiddenItems, setHiddenItems] = useState([]);
 
   const calculateMenuItems = () => {
-    const containerWidth = document.getElementById('menu-container').offsetWidth;
-    console.log(containerWidth)
-    const menuItemWidth = 150; // Set your desired width for each menu item
+    const containerWidth =
+      document.getElementById("menu-container").offsetWidth;
+    console.log(containerWidth);
+    const menuItemWidth = 150;
 
     const maxVisibleItems = Math.floor(containerWidth / menuItemWidth);
     setVisibleItems(menuItems.slice(0, maxVisibleItems));
@@ -18,51 +19,46 @@ const DynamicNavigationMenu = ({ menuItems }) => {
 
   useEffect(() => {
     calculateMenuItems();
-    window.addEventListener('resize', calculateMenuItems);
+    window.addEventListener("resize", calculateMenuItems);
 
     return () => {
-      window.removeEventListener('resize', calculateMenuItems);
+      window.removeEventListener("resize", calculateMenuItems);
     };
   }, [menuItems]);
 
-
-
   return (
-    <div className='nac'>
-    
-    <div id="menu-container" className="menu-container">
-    <div>
-    <img className='imga' src={a}></img>
-    </div>
-      <ul className="main-menu">
-        {visibleItems.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-        {hiddenItems.length > 0 && (
+    <div className="nac">
+      <div id="menu-container" className="menu-container">
+        <div>
+          <img className="icon-image" src={a}></img>
+        </div>
+        <ul className="main-menu">
+          {visibleItems.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+          {hiddenItems.length > 0 && (
             <>
-            <div class="dropdown">
-  <li class="dropbtn">More</li>
-  <div class="dropdown-content">
-  {hiddenItems.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-    {/* <a href="#">Link 1</a>
+              <div class="dropdown">
+                <li class="dropbtn">More</li>
+                <div class="dropdown-content">
+                  {hiddenItems.map((item, index) => (
+                    <li key={index}>{item}</li>
+                  ))}
+                  {/* <a href="#">Link 1</a>
     <a href="#">Link 2</a>
     <a href="#">Link 3</a> */}
-  </div>
-</div>
-          {/* <div className="more-items" onClick={handleToggleMore}>
+                </div>
+              </div>
+              {/* <div className="more-items" onClick={handleToggleMore}>
             More
           </div> */}
-          </>
-        )}
-      </ul>
-      <div className='inp'>
-        <input type='search' placeholder='Type Here'></input>
+            </>
+          )}
+        </ul>
+        <div className="inp">
+          <input type="search" placeholder="Type Here"></input>
+        </div>
       </div>
-
-    
-    </div>
     </div>
   );
 };
